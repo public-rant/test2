3f1a25cd2c95d22bbc99b8ce0e2efc82488f115a
Richard Anthony <rant@rant-2.local>
2022-12-18 09:08:30 +1100

    set config:

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [reac1t()],
+  site: 'https://public-rant.github.io',
+  base: '/test2',
 });
